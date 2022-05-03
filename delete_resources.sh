52308091c2b5e9eaa15aa8ece82545d6fec26b5f
opheliah95 <opheliah95@gmail.com>
2022-05-03 20:07:23 +0100

    able to access resource id and type

--- delete_resources.sh
+++ delete_resources.sh
@@ -13,7 +13,17 @@ do
         # now delete the resource
         for resource in ${result_refined}
         do
-            echo ${resource} | cut -d ":" -f6
+            # find last col which contains id
+            id=$(echo ${resource} | awk -F: '{print $NF}' | awk -F/ '{print $NF}')
+            echo "$id identified"
+            
+            # get the resource type
+            len=${#id} # the : at the end
+            len=$(($len + 1)) 
+            echo "the length of string to be cut is: $len"
+            r_type=$(echo ${resource::-len}| awk -F: '{print $NF}')
+            echo "resource type is $r_type"
+
         done
     else
         printf "result is empty for ${region} \n\n"
