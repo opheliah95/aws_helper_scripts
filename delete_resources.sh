2d83e14a0076c1e5fd4586ff2ae02e43e2ac427d
opheliah95 <opheliah95@gmail.com>
2022-05-03 20:26:42 +0100

    feat: added function to try delete resources

--- delete_resources.sh
+++ delete_resources.sh
@@ -21,8 +21,30 @@ do
             len=${#id} # the : at the end
             len=$(($len + 1)) 
             echo "the length of string to be cut is: $len"
-            r_type=$(echo ${resource::-len}| awk -F: '{print $NF}')
+            r_type=$(echo ${resource::-len}| awk -F: '{print $NF}') 
             echo "resource type is $r_type"
+
+            # run the delete command
+            if [[ $r_type == "subnet" ]]
+            then
+                echo "Now running: aws $r_type delete-resource --resource-id $id"
+                aws ec2 delete-subnet --subnet-id $id
+            
+            elif [[ $r_type == "vpc" ]]
+            then
+                echo "terminating ec2 instance $id"
+                aws ec2 delete-vpc --vpc-id $id
+
+            elif [[ $r_type == "ec2" ]]
+            then
+                echo "terminating vpc instance $id"
+                aws ec2 terminate-instances --instance-ids $id
+
+            else
+                echo "function for delete ${r_type} is not yet impelemted"
+
+            fi
+
 
         done
     else
